052e612b3efda3f32934e61b14278adbd59d4574
Simon Hardt <128412483+thescriptkiddy@users.noreply.github.com>
2025-01-31 12:41:52 +0100

    Prototpye of card layout for competitions

--- backend/app/home/routes.py
+++ backend/app/home/routes.py
@@ -1,5 +1,9 @@
 from flask import render_template
+from sqlalchemy import select
+
 from backend.app.home import bp
+from backend.models.item import Item
+from shared.database import db_session
 
 
 @bp.route('/')
@@ -10,4 +14,6 @@ def index():
 @bp.route('/testing-templates')
 def home_testing_templates():
     """Just a route for testing"""
-    return render_template("home/bootstrap-table.html")
+    result = db_session.execute(select(Item))
+    items = result.scalars().all()
+    return render_template("components/card_competition.html", items=items)
